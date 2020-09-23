a07e74999ca707c9aac9e6bb632eeaee285f7730
Tabatha Slatton <valbatha@gmail.com>
2020-09-23 11:21:13 -0600

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,12 @@
+
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+
+  @@cart = []
+  # This should check to see if that item is in @@items and then add it to the cart if it is. 
+  #           Otherwise give an error
+
 
   def call(env)
     resp = Rack::Response.new
@@ -13,8 +19,25 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.length > 0
+        @@cart.each do |cart|
+        resp.write "#{cart}\n"
+        end
+      else
+        resp.write "Your cart is empty"
+      end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if @@items.include?(item)
+        @@cart << item
+        resp.write "added #{item}"
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
+
     end
 
     resp.finish
